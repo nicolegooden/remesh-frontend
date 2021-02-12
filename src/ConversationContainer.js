b986f92233fee651a46474c190dc08980d9b55b0
Nicole Gooden <nicolemgooden.dev@gmail.com>
2021-02-12 14:04:07 -0700

    Render Conversation component for each element in conversations array

--- src/ConversationContainer.js
+++ src/ConversationContainer.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Conversation from './Conversation.js';
 
 const ConversationsContainer = (props) => {
+ 
   const conversations = props.conversations.map(conv => {
-    const startDate = conv.start_date.split('T')[0]
+    const startDate = conv.start_date.split('T')[0];
     return (
-      <div>
-        <p>{conv.title}</p>
-        <p>{startDate}</p>
-      </div>
+      <Conversation 
+        conversationID={conv.conversation_id}
+        startDate={startDate}
+        title={conv.title}
+      />
     )
   })
+
   return (
     <section>
       <h1>Let's Start a Conversation...</h1>

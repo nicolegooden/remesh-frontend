83363710ed56db24286fcac9541176402d120243
Nicole Gooden <nicolemgooden.dev@gmail.com>
2021-02-12 15:28:05 -0700

    Format dateSent for each message and thought

--- src/MessageContainer.js
+++ src/MessageContainer.js
@@ -4,11 +4,13 @@ import Message from './Message.js';
 const MessageContainer = (props) => {
   const { messages } = props;
   const messageSection = messages.map(message => {
+    const dateSent = message.date_sent.split('T')[0];
     return (
       <Message 
         messageID={message.message_id}
         text={message.text}
         key={message.message_id}
+        dateSent={dateSent}
       /> 
     )
   })
--- src/ThoughtContainer.js
+++ src/ThoughtContainer.js
@@ -4,11 +4,13 @@ import Thought from './Thought.js';
 const ThoughtContainer = (props) => {
   const { thoughts } = props;
   const thoughtSection = thoughts.map(thought => {
+    const dateSent = thought.date_sent.split('T')[0];
     return (
       <Thought 
         thoughtID={thought.thought_id}
         text={thought.text}
         key={thought.thought_id}
+        dateSent={dateSent}
       />
     )
   })

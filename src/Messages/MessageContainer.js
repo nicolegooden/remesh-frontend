aa1af45820be0274fbcf6ade6b0be49df983a329
Nicole Gooden <nicolemgooden.dev@gmail.com>
2021-02-13 13:36:24 -0700

    Add functionality to POST new message and display it

--- src/Messages/MessageContainer.js
+++ src/Messages/MessageContainer.js
@@ -1,22 +1,49 @@
 import React, { useState } from 'react';
 import Message from './Message.js';
+import { postMessage } from '../apiCalls.js';
 
 const MessageContainer = (props) => {
-  const { messages } = props;
-  const messageSection = messages.map(message => {
-    const dateSent = message.date_sent.split('T')[0];
-    return (
-      <Message 
-        messageID={message.message_id}
-        text={message.text}
-        key={message.message_id}
-        dateSent={dateSent}
-      /> 
-    )
-  })
+  const { messages, conversationID } = props;
+  const [messageText, setText] = useState('');
+
+  const messageSection = () => {
+    if (messages.length > 0) {
+    return messages.map(message => {
+      const dateSent = message.date_sent.split('T')[0];
+      return (
+        <Message 
+          messageID={message.message_id}
+          text={message.text}
+          key={message.message_id}
+          dateSent={dateSent}
+        /> 
+      )
+    })
+  }}
+
+  const trackText = (e) => {
+    setText(e.target.value);
+  }
+
+  const submitMessage = async (e) => {
+    e.preventDefault();
+    const message = {
+      text: messageText
+    }
+    await postMessage(message, conversationID);
+    setText('');
+  }
 
   return (
-    <section>{messageSection}</section>
+    <section>
+      <input 
+        placeholder='message' 
+        value={messageText}
+        onChange={trackText}
+      />
+      <button onClick={submitMessage}>ADD</button>
+      {messageSection()}
+    </section>
   )
 }
 

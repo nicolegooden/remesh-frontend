e6e89d7868ab652762d5c0a856413bfb8ff84606
Nicole Gooden <nicolemgooden.dev@gmail.com>
2021-02-13 14:38:49 -0700

    Add functionality to add a thought to an existing message

--- src/Thoughts/ThoughtContainer.js
+++ src/Thoughts/ThoughtContainer.js
@@ -1,22 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Thought from './Thought.js';
+import { postThought, getThoughts } from '../apiCalls.js';
 
 const ThoughtContainer = (props) => {
-  const { thoughts } = props;
-  const thoughtSection = thoughts.map(thought => {
-    const dateSent = thought.date_sent.split('T')[0];
-    return (
-      <Thought 
-        thoughtID={thought.thought_id}
-        text={thought.text}
-        key={thought.thought_id}
-        dateSent={dateSent}
-      />
-    )
-  })
+  const { thoughts, messageID, setThoughts } = props;
+  const [thoughtText, setText] = useState('');
+
+  const thoughtSection = () => { 
+    if (thoughts.length > 0) {
+      return thoughts.map(thought => {
+        const dateSent = thought.date_sent.split('T')[0];
+        return (
+          <Thought 
+            thoughtID={thought.thought_id}
+            text={thought.text}
+            key={thought.thought_id}
+            dateSent={dateSent}
+          />
+        )
+      })
+    }
+  }
+
+  const trackText = (e) => {
+    setText(e.target.value);
+  }
+
+  const submitThought = async (e) => {
+    e.preventDefault();
+    const thought = {
+      text: thoughtText
+    }
+    await postThought(thought, messageID);
+    getThoughts(props.messageID)
+    .then(data => setThoughts(data)) 
+    setText('');
+  }
 
   return (
-    <section>{thoughtSection}</section>
+    <section>
+      <input 
+        placeholder='thought' 
+        value={thoughtText}
+        onChange={trackText}
+      />
+      <button onClick={submitThought}>ADD</button>
+      {thoughtSection()}
+    </section>
   )
 }
 
